bea20f82c72aa633b2cdb4648e03999d936f13b8
mhlnu <alex@mhl.nu>
2020-08-17 16:52:45 +0100

    URL fix

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <Segment className='footer'>
             <Container className='pt1 pb1'>
-                <p>Proiect open-source de <a href='https://www.subiectiv.ro'>Alex Mihăileanu</a>. Cod disponibil pe <a href='https://github.com/mhlnu/covidro'>GitHub</a>.
+                <p>Proiect open-source de <a href='https://www.subiectiv.ro'>Alex Mihăileanu</a>. Cod disponibil pe <a href='https://github.com/mhlnu/covidro_frontend'>GitHub</a>.
                 <br />Sursa datelor: <a href='https://www.datelazi.ro'>datelazi.ro</a> și <a href='https://www.graphs.ro'>graphs.ro</a>.</p>
             </Container>
         </Segment>
